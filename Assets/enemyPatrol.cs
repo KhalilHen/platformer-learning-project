a8f6094294895e98b81a0dd63cbdbece2b42ebb9
Khalil <khalilhenane@hotmail.com>
2024-08-04 17:29:00 +0200

    Made a enemy patrol  and tried working  on  die function and jump

--- Assets/enemyPatrol.cs
+++ Assets/enemyPatrol.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class enemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed = 2f;
+    public float jumpForce = 5f;  // Adjust the jump force as needed
+    public float jumpInterval = 5f; // Time in seconds between jumps
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Transform currentPoint;
+    private float nextJumpTime; // Time when the next jump should occur
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        animator.SetBool("isRunning", true);
+
+        // Initialize next jump time
+        nextJumpTime = Time.time + jumpInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+        // Check if the enemy needs to change direction
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+        {
+            if (currentPoint == pointB.transform)
+            {
+                currentPoint = pointA.transform;
+            }
+            else
+            {
+                currentPoint = pointB.transform;
+            }
+        }
+
+        // Handle jumping
+        if (Time.time >= nextJumpTime && IsGrounded())
+        {
+            Jump();
+            nextJumpTime = Time.time + jumpInterval; // Set the next jump time
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private bool IsGrounded()
+    {
+        // Check if the enemy is grounded by overlapping a small circle
+        return Physics2D.OverlapCircle(transform.position - new Vector3(0, 0.5f, 0), 0.2f, LayerMask.GetMask("Ground"));
     }
 }
--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,52 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-
-
-
-
-    // Start is called before the first frame update
-   
-    //movement code
     private float horizontal;
-
     public float speed = 8f;
     public float jumpingPower = 8f;
-    //private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     [SerializeField] Rigidbody2D rb;
-    public double movementSpeed = 1f;
-    private GameObject platform; // Added to store the platform GameObject
-
-
+    private GameObject platform;
 
     void Start()
     {
-
-
-
-
-
-        //Code for finding the platform object
         Transform objectsParent = GameObject.Find("objects").transform;
-
         if (objectsParent != null)
         {
-            // Find the 'platform' GameObject within 'objects'
             Transform platformTransform = objectsParent.Find("platform");
-
             if (platformTransform != null)
             {
-                GameObject platform = platformTransform.gameObject;
-                // Now 'platform' is the reference to the 'platform' GameObject
+                platform = platformTransform.gameObject; // Assign the platform reference
                 Debug.Log("Found platform: " + platform.name);
             }
             else
@@ -60,101 +37,70 @@ public class movement : MonoBehaviour
         }
     }
 
-        // Update is called once per frame
-        void Update()
+    void Update()
     {
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //Movement code
-
         horizontal = Input.GetAxisRaw("Horizontal");
-
-
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-       if(Mathf.Abs(horizontal) > 0 && isGrounded() && platform == true)
+        if (Mathf.Abs(horizontal) > 0 && isGrounded() && platform != null)
         {
-
-
             Flip();
         }
-        //Calls the jump method
-       jump();
 
-  
-
-
+        jump();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
+
     public void Flip()
     {
-
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
-
-
         }
     }
 
     private bool isGrounded()
     {
-        
-            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
-        
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
-    
+
     public void jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded() )
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-
-
-
         }
-
 
         if (Input.GetButtonUp("jump") && rb.velocity.y > 0f)
         {
-
-
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-
         }
-
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision is with an enemy
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Handle character death
+            Die();
+        }
+    }
 
-
+    private void Die()
+    {
+        // Handle death logic here
+        Debug.Log("Character has died!");
+        // Optionally, you can reload the scene or disable the character
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        // Or
+        // gameObject.SetActive(false);
+    }
 }
