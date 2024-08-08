c99868a53e15900ab74aca6b402c9f6347075ec3
Khalil <khalilhenane@hotmail.com>
2024-08-08 16:40:19 +0200

    created a simple main menu, if you click on play it loads the game

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -155,15 +155,7 @@ public class movement : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Check if the collision is with an enemy
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // Handle character death
-            Die();
-        }
-    }
+  
     private void Die()
     {
         // Handle death logic here
